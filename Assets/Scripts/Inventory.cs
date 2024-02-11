70e34841a6441077a57e87028b44ad59d9ec6822
Andrei <142443853+hjzres@users.noreply.github.com>
2024-02-11 17:26:04 -0500

    Removed KeyScriptableObject + made the inventory switch slots

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public Item[] inventory = new Item[4];
+    public Item[] Items = new Item[4];
+    public Item selectedItem;
 
-    private void Start(){
-        
-    }    
+    private void Update(){
+        if(numberClicked() != 0){
+            selectedItem = Items[numberClicked() - 1];
+        }
+        Debug.Log(selectedItem);
+    }
+
+    private int numberClicked(){
+        if(Input.GetKeyDown("1")){
+            return 1;
+        }
+        if(Input.GetKeyDown("2")){
+            return 2;
+        }
+        if(Input.GetKeyDown("3")){
+            return 3;
+        }
+        if(Input.GetKeyDown("4")){
+            return 4;
+        }
+        return 0;
+    }
 }
