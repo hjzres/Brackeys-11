f37a72ac09448a06345831bf41f2f1136a5932c4
Andrei <142443853+hjzres@users.noreply.github.com>
2024-02-11 16:19:44 -0500

    Made a Inventory script

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Item[] inventory = new Item[4];
+
+    private void Start(){
+        
+    }    
+}
