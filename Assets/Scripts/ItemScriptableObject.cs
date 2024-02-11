5e94f75914708f7fa227a563b67d6a7ccb61a9a2
Andrei <142443853+hjzres@users.noreply.github.com>
2024-02-11 15:59:09 -0500

    Made the scriptable object for "Item"

---
+++ Assets/Scripts/ItemScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    [SerializeField] private string itemName;
+    [SerializeField] private Sprite itemImage;
+    [SerializeField] private string itemDescription;
+}
