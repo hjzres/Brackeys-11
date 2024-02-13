147c3e89c80e36ab7d484c8a1be3ce70aa1cf9aa
Andrei <142443853+hjzres@users.noreply.github.com>
2024-02-13 09:35:07 -0500

    Buttons from the main pause menu function

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
-{
+{   
+    public void resumeScene(){
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+    }
 
     public void changeScene(string sceneName)
     {
