bb3a274ccdc01b0b4a5616c33da006845b56b95f
winterSteve25 <W1nter.IsH3re@gmail.com>
2024-02-17 13:37:53 -0500

    Prevented door from opening when closing and vice versa

--- Assets/Scripts/RoomSpecific/AutoDoor.cs
+++ Assets/Scripts/RoomSpecific/AutoDoor.cs
@@ -12,6 +12,7 @@ namespace RoomSpecific
         [SerializeField] private float range;
 
         private bool _wasOpen;
+        private bool _inProgress;
 
         private void Start()
         {
@@ -20,6 +21,8 @@ namespace RoomSpecific
 
         private void Update()
         {
+            Debug.Log(_wasOpen);
+            
             Vector3 playerPos = PlayerInstance.Instance.transform.position;
             
             bool inRangeOfDoor = (playerPos - transform.position).magnitude <= range;
@@ -39,27 +42,36 @@ namespace RoomSpecific
             }
             else if (_wasOpen)
             {
-                _wasOpen = false;
                 Close();
             }
         }
 
         private void Open()
         {
+            if (_inProgress) return;
+            _inProgress = true;
             portal.gameObject.SetActive(true);
             doorWing.enabled = false;
             doorWing.transform.DOLocalRotate(new Vector3(0, 120, 0), 0.5f)
-                .OnComplete(() => { doorWing.enabled = true; });
+                .OnComplete(() =>
+                {
+                    doorWing.enabled = true;
+                    _inProgress = false;
+                });
         }
 
         private void Close()
         {
+            if (_inProgress) return;
+            _inProgress = true;
             doorWing.enabled = false;
             doorWing.transform.DOLocalRotate(new Vector3(0, 0, 0), 0.5f)
                 .OnComplete(() =>
                 {
                     portal.gameObject.SetActive(false);
                     doorWing.enabled = true;
+                    _inProgress = false;
+                    _wasOpen = false;
                 });
         }
     }
